015c72f37b0a92179a54a4427c354c3cbd311f73
sc23ade <sc23ade@herts.ac.uk>
2024-12-11 03:04:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display a welcome message on page load
+window.onload = function () {
+    console.log("Welcome to Sana's profile site!");
+
+    // Display a greeting alert to the user
+    alert("Welcome to my professional profile site!");
+};
